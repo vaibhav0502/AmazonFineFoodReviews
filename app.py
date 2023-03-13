783e49a57ccf2ea0ba1dad70724878ea30a86467
vaibhav0502 <kumbharvaibhav007@gmail.com>
2023-03-13 19:01:32 +0530

    updated flask app

--- app.py
+++ app.py
@@ -9,19 +9,21 @@ MODEL = pickle.load(open(os.path.normpath('models/model.pkl'), 'rb'))
 WORD_VECTORIZER = pickle.load(open(os.path.normpath ("models/word_vectorizer.pkl"), 'rb'))
 CLEAN_OBJ = TextProcess()
 
-MAPPING = {'0': 'Negative', '1': 'Positive'}
+MAPPING = {'1': 'Negative', '0': 'Positive'}
 
 def get_sentiment(text):
   vec = WORD_VECTORIZER.transform([text])
-  pred = MODEL.predict(vec)
-  return pred[0]
+#   pred = MODEL.predict(vec)
+  pred = MODEL.predict_proba(vec)[:, 1][0]
+  threshold = 0.7
+  return [1 if pred > threshold else 0][0]
 
 @app.route('/', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         # text = request.json['text']
-        text = request.form['text'].lower()
-        text1 = CLEAN_OBJ.clean_text(text)
+        text = request.form['text']
+        text1 = CLEAN_OBJ.clean_text(text.lower())
         score = get_sentiment(text1)
         sentiment = MAPPING[str(score)]
         return render_template('form.html', final=True, text=text, sentiment=sentiment)
